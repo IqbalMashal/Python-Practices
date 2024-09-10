c786ad4b56630e0185255a06f3814d44291d1b62
Iqbal Mashal <151959357+IqbalMashal@users.noreply.github.com>
2024-09-10 10:51:15 -0400

    Update star_pattern01.py

--- star_pattern01.py
+++ star_pattern01.py
@@ -1,12 +1,19 @@
 
 
+# # Right Triangle Star Pattern
+# # *
+# # **
+# # ***
+# # ****
+# # *****
+
+
+
 print("Enter the character for the pattern: ", end='')
 char = input()
 
 print("Enter the number for the pattern: ", end='')
 num = int(input())
-
-print ("\n")
 
 for i in range(num):
     print(char * (i + 1))  # Print the character repeated (i + 1) times
