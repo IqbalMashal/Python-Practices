e2bd696e94f1942a572e38fc28799ddbb567149b
Iqbal Mashal <151959357+IqbalMashal@users.noreply.github.com>
2024-09-11 14:15:29 -0400

    Add files via upload

---
+++ python_calculator.py
@@ -0,0 +1,24 @@
+
+
+
+operator = input("Enter an operator (+ - * /): ")
+
+num1 = float(input("Enter the 1st number: "))
+num2 = float(input("Enter the 2nd number: "))
+
+
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    result = num1 / num2 
+else:
+    print(f"{operator} is not a valid operator")
+# f-string (f"{operator} is not a valid operator"): 
+# This is a formatted string literal (f-string) that allows 
+# you to embed expressions inside string literals using curly braces {}.
+
+print (round(result,2))
